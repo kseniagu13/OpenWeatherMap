f97054e60f62c6d23166ae0a65fb8b3a8140b0f6
Ksenia Gustsenko <kseniagustsenko@Ksenias-MacBook-Air.local>
2020-01-17 15:00:17 +0200

    Weather

---
+++ OpenWeatherMap/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace OpenWeatherMap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string url = "https://openweathermap.org/data/2.5/weather?id=588409&appid=b6907d289e10d714a6e88b30761fae22";
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "GET";
+            var webResponse = request.GetResponse();
+            var webStream = webResponse.GetResponseStream();
+
+            using (var responseReader = new StreamReader(webStream))
+            {
+                var response = responseReader.ReadToEnd();
+                WeatherDetails.root weather = JsonConvert.DeserializeObject<WeatherDetails.root>(response);
+                Console.WriteLine(weather.Name);
+                Console.WriteLine(weather.Sys.Country);
+                Console.WriteLine(weather.Weather[0].Main); // Maini sees on massiiv, kus sees on ainult üks väärtus
+                Console.WriteLine(weather.Weather[0].Description);// Descriptioni omaduse sees on ainult üks väärtus
+                Console.WriteLine(weather.Main.Temp) ;
+
+
+            }
+        }
+    }
+}
---
+++ OpenWeatherMap/WeatherDetails.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+namespace OpenWeatherMap
+{
+   class WeatherDetails
+    {
+        public class Weather //selle klassi sees on järgmised omadused:
+        {
+            public string Main { get; set; }
+            public string Description { get; set; }
+        }
+        public class Sys
+        {
+            public string Country { get; set; }
+
+        }
+        public class Main
+        {
+            public double Temp { get; set; }
+        }
+        
+        public class root 
+        {
+          public string Name { get; set; } //name asub üldfili sees, ehk rootis
+          public List <Weather> Weather { get; set; }
+          public Sys Sys { get; set; }
+         public Main Main { get; set; }
+        }
+        
+    }
+}
